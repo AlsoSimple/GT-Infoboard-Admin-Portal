25c1cfb7af7b5f26e998e3803417009a060acfe7
MalteEngelund <malte.engelund@hotmail.com>
2026-03-09 12:28:18 +0100

    Update LoginPage.tsx

--- GT-InfoBoard-Admin-Portal/src/pages/LoginPage/LoginPage.tsx
+++ GT-InfoBoard-Admin-Portal/src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,13 @@
 import style from './LoginPage.module.scss'
 
 import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 //fjern udkommentering når router er sat op, og tilføj import for useNavigate
 
 export function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,7 +21,7 @@ export function LoginPage() {
     console.log('Login response:', data);
     if (response.ok && data.token) {
       localStorage.setItem('token', data.token);
-      // navigate('/home');
+      navigate('/home');
     } else {
       // Handle login failure (e.g., show error message)
       alert('Login failed: ' + (data.message || 'Unknown error'));
